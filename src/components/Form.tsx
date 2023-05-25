682c64315b167c80a42d22c325983a2b16864357
Waldemar Glaz <ania@Waldemars-MacBook-Pro.local>
2023-05-25 10:47:45 +0200

    react hook form validation applied

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -3,6 +3,16 @@ import { useForm } from "react-hook-form";
 import { DevTool } from "@hookform/devtools";
 import axios from "axios";
 
+type FormValues = {
+  name: string;
+  type: string;
+  preparation_time: string;
+  no_of_slices: string;
+  diameter: string;
+  spiciness_scale: string;
+  slices_of_bread: string;
+};
+
 const Form = () => {
   const [isSending, setIsSending] = useState(false);
   const [success, setSuccess] = useState(false);
@@ -18,9 +28,18 @@ const Form = () => {
       slices_of_bread: "",
     },
   };
-  const form = useForm(defaultValues);
-  const { register, control, handleSubmit, watch, unregister, reset } = form;
+  const form = useForm<FormValues>(defaultValues);
+  const {
+    register,
+    control,
+    handleSubmit,
+    watch,
+    unregister,
+    reset,
+    formState,
+  } = form;
   const watchType = watch("type");
+  const { errors } = formState;
   useEffect(() => {
     if (watchType === "pizza") {
       register("no_of_slices");
@@ -36,7 +55,7 @@ const Form = () => {
       unregister("slices_of_bread");
     }
   }, [register, unregister, watchType]);
-  const onSubmit = (data: any) => {
+  const onSubmit = (data: FormValues) => {
     setIsSending(true);
     const url =
       "https://umzzcc503l.execute-api.us-west-2.amazonaws.com/dishes/";
@@ -61,20 +80,30 @@ const Form = () => {
   return (
     <>
       {!isSending && !error && !success && (
-        <form onSubmit={handleSubmit(onSubmit)}>
+        <form onSubmit={handleSubmit(onSubmit)} noValidate>
           <label htmlFor="name">Dish name:</label>
-          <input type="text" id="name" {...register("name")} required />
+          <input
+            type="text"
+            id="name"
+            {...register("name", { required: "Dish name is required" })}
+          />
+          <p>{errors.name?.message}</p>
           <label htmlFor="preparation_time">Preparation time:</label>
           <input
             type="time"
             id="preparation-time"
             step="2"
-            required
-            {...register("preparation_time")}
+            {...register("preparation_time", {
+              required: "Preparation time is required",
+            })}
           />
+          <p>{errors.preparation_time?.message}</p>
           <label htmlFor="type">
             Dish type:
-            <select {...register("type", { required: true })}>
+            <select
+              required
+              {...register("type", { required: "Dish type is required" })}
+            >
               <option value="" disabled>
                 Choose here
               </option>
@@ -82,6 +111,7 @@ const Form = () => {
               <option value="soup">Soup</option>
               <option value="sandwich">Sandwich</option>
             </select>
+            <p>{errors.type?.message}</p>
           </label>
           {watchType === "pizza" && (
             <>
@@ -91,18 +121,24 @@ const Form = () => {
                   type="number"
                   id="number-of-slices"
                   required
-                  {...register("no_of_slices")}
+                  {...register("no_of_slices", {
+                    min: 1,
+                    required: "Number of slices is required",
+                  })}
                 />
+                <p>{errors.no_of_slices?.message}</p>
               </label>
               <label htmlFor="diameter">
                 Diameter:
                 <input
                   type="number"
                   id="diameter"
-                  required
-                  {...register("diameter")}
+                  {...register("diameter", {
+                    required: "Diameter is required",
+                  })}
                   step="0.01"
                 />
+                <p>{errors.diameter?.message}</p>
               </label>
             </>
           )}
@@ -113,13 +149,10 @@ const Form = () => {
                 <input
                   type="number"
                   id="spicyness-scale"
-                  min="1"
-                  max="10"
-                  required
                   {...register("spiciness_scale", {
                     min: 1,
                     max: 10,
-                    required: true,
+                    required: "Spicyness scale is required",
                   })}
                 />
               </label>
@@ -131,9 +164,11 @@ const Form = () => {
               <input
                 type="number"
                 id="slices_of_bread"
-                required
-                {...register("slices_of_bread")}
+                {...register("slices_of_bread", {
+                  required: "this field is required",
+                })}
               />
+              <p>{errors.slices_of_bread?.message};</p>
             </label>
           )}
           <button type="submit">Add</button>
