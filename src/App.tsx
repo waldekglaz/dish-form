3bb5d6cff43fc80b344bfa28b5483e6b492df0db
Waldemar Glaz <ania@Waldemars-MacBook-Pro.local>
2023-05-28 12:58:04 +0200

    box shadow added

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Heading from "./components/Heading";
 
 const App = () => {
   return (
-    <main className="p-4 ">
+    <main className="max-w-xs mx-auto p-4 shadow-md mt-16 rounded-lg">
       <Heading title="Dish Delight" />
       <Form />
     </main>
